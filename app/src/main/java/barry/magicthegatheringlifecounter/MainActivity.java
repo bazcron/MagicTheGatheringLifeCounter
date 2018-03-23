96b38c8cd70453e2c0d803f2faa625e932e6c11e
bazcron <bazcron@yahoo.co.uk>
2018-03-23 00:41:29 +0000

    radio buttons working

--- app/src/main/java/barry/magicthegatheringlifecounter/MainActivity.java
+++ app/src/main/java/barry/magicthegatheringlifecounter/MainActivity.java
@@ -2,17 +2,24 @@ package barry.magicthegatheringlifecounter;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Layout;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+    static View theView;
+    static String backgroundImage="";
     private Button leagueTableButton;
     private Button setUpGameButton;
     @Override
@@ -21,6 +28,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // reworked code from this video https://www.youtube.com/watch?v=RrAxLCIMj6s
+        theView = this.getWindow().getDecorView();
+        switch (backgroundImage){
+            case "red": theView.setBackgroundResource(R.drawable.red);
+            break;
+            case "white": theView.setBackgroundResource(R.drawable.white);
+                break;
+            case "black": theView.setBackgroundResource(R.drawable.black);
+                break;
+            case "blue": theView.setBackgroundResource(R.drawable.blue);
+                break;
+            case "green": theView.setBackgroundResource(R.drawable.green);
+                break;
+            default: theView.setBackgroundResource(R.drawable.magic_background);
+                break;
+        }
+
 
         leagueTableButton = (Button) findViewById(R.id.leagueTableButton);
 
@@ -48,13 +73,23 @@ public class MainActivity extends AppCompatActivity {
         // Check which radio button was clicked
         switch(view.getId()) {
             case R.id.radio_light:
-                if (checked)
-                    //
+                if (checked) {
+                   /* Toast.makeText(this, "RED",
+                            Toast.LENGTH_LONG).show();*/
+
+                    CoordinatorLayout layout = (CoordinatorLayout) findViewById(R.id.main);
+                    layout.setBackgroundResource(R.drawable.white);
                     break;
+                }
             case R.id.radio_dark:
-                if (checked)
-                    //
+                if (checked){
+                   /* Toast.makeText(this, "RED",
+                            Toast.LENGTH_LONG).show();*/
+
+                    CoordinatorLayout layout = (CoordinatorLayout) findViewById(R.id.main);
+                    layout.setBackgroundResource(R.drawable.black);
                     break;
+                }
         }
     }
 
